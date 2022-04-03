7190c869d4d44b614f39ba7d1e276a542e596bd0
Andrew Healey <doolie.healey@gmail.com>
2022-04-03 04:57:59 -0400

    Add var words

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use ressa::Parser;
 use std::io::{self,Write};
 use resw::Writer;
 
+use rand::{thread_rng,seq::SliceRandom};
+use std::iter::{Iterator,Cycle};
+
 use std::time::{Duration,Instant};
+
+use std::borrow::Cow;
 
 use resast::prelude::*;
 use resast::expr::Expr;
@@ -12,6 +17,10 @@ use std::fs;
 use std::fmt::Debug;
 
 use std::string::String;
+
+fn var_names<'a>(word:&'a str)->Vec<&'a str>{
+    word.split(",").collect()
+}
 
 #[derive(Debug)]
 enum Line<'a> {
@@ -55,6 +64,22 @@ impl<'a> Block<'a> {
         }).collect();
         Program::Script(parts) 
     }
+    pub fn varify(mut self,blocker:&'a mut Blocker)->Block<'a>{
+        let value=self.value();
+        match value {
+            None=>self,
+            Some(expr)=>{
+                let mut action_lines=self.lines();
+                let var_name=blocker.next().expect("Oh no. You're out of variable names.");
+                action_lines.push(Line::Variable(var_name.to_string(),expr));
+                let out_val=Some(Expr::Ident(Ident{name:var_name.into()}));
+                Block {
+                    action_lines,
+                    value:out_val
+                }
+            }
+        }
+    }
 }
 
 // Any ResAST enum/struct that can be converted into a Block.
@@ -65,88 +90,115 @@ enum Blockable<'a> {
     Expr(Expr<'a>)
 }
 
-fn blockify<'a>(blockable:Blockable<'a>)->Block<'a>{
-    match blockable {
-        Blockable::Program(prog)=>{
-            let parts=match prog {
-                Program::Script(parts)=>parts,
-                Program::Mod(parts)=>parts,
-            };
-            let lines:Vec<Line<'a>>=parts.into_iter().map(|part|
-                blockify(Blockable::ProgramPart(part)).lines()
-            ).flatten().collect();
-            Block {
-                action_lines:lines,
-                value:None
-            }
-        },
-        Blockable::ProgramPart(part)=>{
-            match part {
-                ProgramPart::Stmt(stmt)=>blockify(Blockable::Stmt(stmt)),
-                part=>Block{
-                    action_lines:vec![Line::Effectless(part)],
+#[derive(Clone)]
+struct Blocker<'a>{
+    var_names:Vec<&'a str>,
+    idx:usize
+}
+
+impl<'a> Iterator for Blocker<'a>{
+    type Item=&'a str;
+    fn next(&mut self)->Option<&'a str>{
+        let old_idx=self.idx;
+        self.idx+=1;
+        if(self.idx==self.var_names.len()){
+            self.idx=0;
+        }
+        self.var_names.get(old_idx).map(|&st|st)
+    }
+}
+
+impl<'a,'b:'a> Blocker<'a>{
+    pub fn new(var_names:Vec<&'a str>)->Blocker<'a>{
+        let ret=Blocker {
+            var_names,
+            idx:0
+        };
+        ret//.cycle()
+    }
+    pub fn blockify(&self,blockable:Blockable<'b>)->Block<'b>{
+        match blockable {
+            Blockable::Program(prog)=>{
+                let parts=match prog {
+                    Program::Script(parts)=>parts,
+                    Program::Mod(parts)=>parts,
+                };
+                let lines:Vec<Line<'b>>=parts.into_iter().map(|part|
+                    self.blockify(Blockable::ProgramPart(part)).lines()
+                ).flatten().collect();
+                Block {
+                    action_lines:lines,
                     value:None
                 }
-            }
-        },
-        Blockable::Stmt(stmt)=>{
-            match stmt {
-                Stmt::Expr(expr)=>blockify(Blockable::Expr(expr)),
-                stmt=>Block{
-                    action_lines:vec![Line::Effectless(ProgramPart::Stmt(stmt))],
-                    value:None
+            },
+            Blockable::ProgramPart(part)=>{
+                match part {
+                    ProgramPart::Stmt(stmt)=>self.blockify(Blockable::Stmt(stmt)),
+                    part=>Block{
+                        action_lines:vec![Line::Effectless(part)],
+                        value:None
+                    }
                 }
-            }
-        },
-        Blockable::Expr(expr)=>{
-            match expr {
-                Expr::Sequence(seq)=>{
-                    let mut action_lines:Vec<Line<'a>>=vec![];
-                    let mut value:Option<Expr<'a>>=None;
-
-                    for expr in seq {
-                        let mut expr_block=blockify(Blockable::Expr(expr));
-                        let expr_value=expr_block.value();
-                        let mut expr_lines=expr_block.lines();
-                        value=match value {
-                            Some(expr)=>{
-                                action_lines.push(Line::Effectless(ProgramPart::Stmt(Stmt::Expr(expr))));
-                                expr_value
-                            },
-                            None=>expr_value
-                        };
-                        action_lines.append(&mut expr_lines);
+            },
+            Blockable::Stmt(stmt)=>{
+                match stmt {
+                    Stmt::Expr(expr)=>self.blockify(Blockable::Expr(expr)),
+                    stmt=>Block{
+                        action_lines:vec![Line::Effectless(ProgramPart::Stmt(stmt))],
+                        value:None
                     }
+                }
+            },
+            Blockable::Expr(expr)=>{
+                match expr {
+                    Expr::Sequence(seq)=>{
+                        let mut action_lines:Vec<Line<'b>>=vec![];
+                        let mut value:Option<Expr<'b>>=None;
 
-                    Block {
-                        action_lines,
-                        value
+                        for expr in seq {
+                            let mut expr_block=self.blockify(Blockable::Expr(expr));
+                            let expr_value=expr_block.value();
+                            let mut expr_lines=expr_block.lines();
+                            value=match value {
+                                Some(expr)=>{
+                                    action_lines.push(Line::Effectless(ProgramPart::Stmt(Stmt::Expr(expr))));
+                                    expr_value
+                                },
+                                None=>expr_value
+                            };
+                            action_lines.append(&mut expr_lines);
+                        }
+
+                        Block {
+                            action_lines,
+                            value
+                        }
+                    },
+                    Expr::Call(CallExpr{callee,arguments})=>{
+
+                        let mut callee_block=self.blockify(Blockable::Expr(*callee));
+
+                        let mut arg_blocks:Vec<Block<'b>>=arguments.into_iter().map(|arg|self.blockify(Blockable::Expr(arg))).collect();
+
+                        let mut value:Option<Expr<'b>>=Some(Expr::Call(CallExpr{
+                            callee:Box::new(callee_block.expr()),
+                            arguments:arg_blocks.iter_mut().map(|block|block.expr()).collect()
+                        }));
+
+                        let mut line_blocks=vec![callee_block];
+                        line_blocks.append(&mut arg_blocks);
+
+                        let action_lines=line_blocks.into_iter().map(|block|block.lines()).flatten().collect();
+
+                        Block {
+                            action_lines,
+                            value
+                        }
+                    },
+                    expr=>Block{
+                        action_lines:vec![],
+                        value:Some(expr)
                     }
-                },
-                Expr::Call(CallExpr{callee,arguments})=>{
-
-                    let mut callee_block=blockify(Blockable::Expr(*callee));
-
-                    let mut arg_blocks:Vec<Block<'a>>=arguments.into_iter().map(|arg|blockify(Blockable::Expr(arg))).collect();
-
-                    let mut value:Option<Expr<'a>>=Some(Expr::Call(CallExpr{
-                        callee:Box::new(callee_block.expr()),
-                        arguments:arg_blocks.iter_mut().map(|block|block.expr()).collect()
-                    }));
-
-                    let mut line_blocks=vec![callee_block];
-                    line_blocks.append(&mut arg_blocks);
-
-                    let action_lines=line_blocks.into_iter().map(|block|block.lines()).flatten().collect();
-
-                    Block {
-                        action_lines,
-                        value
-                    }
-                },
-                expr=>Block{
-                    action_lines:vec![],
-                    value:Some(expr)
                 }
             }
         }
@@ -154,6 +206,10 @@ fn blockify<'a>(blockable:Blockable<'a>)->Block<'a>{
 }
 
 fn main() {
+
+    let words="basic,salad,agate,spicy,spray,essay,fjord,spend,kebab,guild,aback,motor,alone,hatch,hyper,thumb,dowry,ought,belch,dutch,pilot,tweed,comet,jaunt,enema,steed,abyss,growl,fling,dozen,boozy,erode,world,gouge,click,briar,great,altar,pulpy,blurt,coast,duchy,groin,fixer,group,rogue,badly";
+    let names=var_names(words);
+    let blocker=Blocker::new(names);
 
     let contents=fs::read_to_string("ex.js").expect("Failed to read");
 
@@ -164,7 +220,7 @@ fn main() {
     let start=Instant::now();
 
     let blockable=Blockable::Program(program);
-    let block=blockify(blockable);
+    let block=blocker.blockify(blockable);
 
     println!("micros: {}",start.elapsed().subsec_micros());
 
